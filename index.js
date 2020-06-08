99677b2d979d8a3bf6ed874c3a96e60dda0b619b
takico <takita.y1026@gmail.com>
2020-06-09 03:33:02 +0900

    index.js実装完了

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write('<!DOCTYPE html><html lang="ja"><body><h1>HTMLの一番大きい見出しを表示します</h1></body></html>');
   res.end();
 });
 const port = 8000;
